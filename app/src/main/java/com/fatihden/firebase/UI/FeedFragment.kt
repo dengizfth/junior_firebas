591992fe0d345a7471ce20d1a18ca898d477cb31
Fatih Dengiz <dengizfth@gmail.com>
2024-08-03 15:27:47 +0300

    popup menu ile kotlin kodunu bir birine bağlama ve gösterme işlemi

--- app/src/main/java/com/fatihden/firebase/UI/FeedFragment.kt
+++ app/src/main/java/com/fatihden/firebase/UI/FeedFragment.kt
@@ -5,14 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import com.fatihden.firebase.R
 import com.fatihden.firebase.databinding.FragmentFeedBinding
-import com.fatihden.firebase.databinding.FragmentUserBinding
+
 
 
 class FeedFragment : Fragment() {
     private var _binding: FragmentFeedBinding? = null
     private val binding get() = _binding!!
+
+    // Pop Up menu decleration
+    private lateinit var popupMenu: PopupMenu
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +30,25 @@ class FeedFragment : Fragment() {
         _binding = FragmentFeedBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // xml'deki bir şeyi koda bağlarken sonu inflater ile biten yapıları kullanırız
+        // layout'u koda bağlayınca LayoutInflater kullandığımız gibi menu içinde inflater var
+        // istediği context e requireContext() verdikten sonra ne ile bağlantı yapıcağız
+        // bağlantı yapılacak yapının id'si verilir -> ,binding.floatingActionButton gibi
+        popupMenu = PopupMenu(requireContext(),binding.floatingActionButton)
+        // bağlamak için menuInflater kullanılır . menu xml koda bağlamak için kullanılır.
+        val inflater = popupMenu.menuInflater
+        //kodu ile xml bağlamak için son olarak :
+        inflater.inflate(R.menu.my_popup_menu,popupMenu.menu)
+        binding.floatingActionButton.setOnClickListener {
+
+            //Görünümde artık gözükmesi için :
+            popupMenu.show()
+
+        }
     }
 
     override fun onDestroy() {
