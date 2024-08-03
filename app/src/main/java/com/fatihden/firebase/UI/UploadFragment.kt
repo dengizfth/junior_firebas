4967f5246b1999b99a550d156d80a64be1a67e84
Fatih Dengiz <dengizfth@gmail.com>
2024-08-04 00:26:00 +0300

    sdk 33 ve yukarı için , 33'ün altnda versionlar için izin şart bloğu

--- app/src/main/java/com/fatihden/firebase/UI/UploadFragment.kt
+++ app/src/main/java/com/fatihden/firebase/UI/UploadFragment.kt
@@ -1,5 +1,6 @@
 package com.fatihden.firebase.UI
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -30,6 +31,22 @@ class UploadFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Resim seçme için api 33 altı , 33 ve üstü için ayrı ayrı kontroller:
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU ){
+            // sdk 33 ve yukarısı için -> read media images izni :
+
+
+        } else {
+            // sdk 33'ün altındaki eski versionlar için -> read external storage :
+
+        }
+
+
+        binding.uploadBtn.setOnClickListener {
+
+
+        }
 
 
     }
