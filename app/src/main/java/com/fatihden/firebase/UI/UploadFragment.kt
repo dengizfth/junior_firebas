77b60c16134ac1d81dd996fbeff7b8105584110f
Fatih Dengiz <dengizfth@gmail.com>
2024-08-05 23:01:17 +0300

    kod satırlarını düzenleme ctrl+shift+alt+l

--- app/src/main/java/com/fatihden/firebase/UI/UploadFragment.kt
+++ app/src/main/java/com/fatihden/firebase/UI/UploadFragment.kt
@@ -1,7 +1,6 @@
 package com.fatihden.firebase.UI
 
 import android.Manifest
-import android.app.Activity
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -11,7 +10,6 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -20,7 +18,7 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.fatihden.firebase.R
+import androidx.fragment.app.Fragment
 import com.fatihden.firebase.databinding.FragmentUploadBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
@@ -32,22 +30,26 @@ import com.google.firebase.storage.ktx.storage
 
 class UploadFragment : Fragment() {
 
-    private var _binding:FragmentUploadBinding? = null
+    private var _binding: FragmentUploadBinding? = null
     private val binding get() = _binding!!
 
     // Yeni activity açmak için kullanıcaz ( Galeriyi açmak gibi )
-    private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+
     // izinler için
-    private lateinit var permissionLauncher:ActivityResultLauncher<String>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+
     // Açılan pencereden seçilen resmin adresi tutmak için
-    var selectedPicture : Uri? = null
+    var selectedPicture: Uri? = null
+
     // Secilen resmi Bitmap olarak tutmak için :
-    var selectedBitmap : Bitmap? = null
+    var selectedBitmap: Bitmap? = null
 
     // Firebase kullanıcı bilgilerini almak için :
-    private lateinit var auth : FirebaseAuth
+    private lateinit var auth: FirebaseAuth
+
     //Kullanıcı media'larını depoya yüklemek için
-    private lateinit var storage : FirebaseStorage
+    private lateinit var storage: FirebaseStorage
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,10 +60,9 @@ class UploadFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentUploadBinding.inflate(inflater,container,false)
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentUploadBinding.inflate(inflater, container, false)
         val view = binding.root
         return view
     }
@@ -70,16 +71,27 @@ class UploadFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         // Resim seçme için api 33 altı , 33 ve üstü için ayrı ayrı kontroller:
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU ){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             // sdk 33 ve yukarısı için -> read media images izni :
-            if (ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_MEDIA_IMAGES
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
                 // İzin yok, izin iste :
 
                 //Daha öncesinde izin istemiyi red etti mi ? kontrol edilmesi :
-                if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),Manifest.permission.READ_MEDIA_IMAGES)) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    )
+                ) {
                     //İzni red etmiş : Bunun için neden izin istediğimizi açıklayarak bir kere daha izin istenilmeli :
-                    Snackbar.make(view , "Galeriden Fotoraf seçmek için vermeniz gerekiyor",Snackbar.LENGTH_INDEFINITE)
-                        .setAction("İzin Ver" , View.OnClickListener {
+                    Snackbar.make(
+                        view,
+                        "Galeriden Fotoraf seçmek için vermeniz gerekiyor",
+                        Snackbar.LENGTH_INDEFINITE
+                    ).setAction("İzin Ver", View.OnClickListener {
                             // İzin İsteyeceğimiz alan :
                             permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
                         }).show()
@@ -91,21 +103,33 @@ class UploadFragment : Fragment() {
             } else {
                 // izin var , telefonun galerisini aç :
                 // Galeriyi aç :
-                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                val intentToGallery =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                 activityResultLauncher.launch(intentToGallery)
             }
 
 
         } else {
             // sdk 33'ün altındaki eski versionlar için -> read external storage :
-            if (ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            if (ContextCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
                 // İzin yok, izin iste :
 
                 //Daha öncesinde izin istemiyi red etti mi ? kontrol edilmesi :
-                if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(),Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    )
+                ) {
                     //İzni red etmiş : Bunun için neden izin istediğimizi açıklayarak bir kere daha izin istenilmeli :
-                    Snackbar.make(view , "Galeriden Fotoraf seçmek için vermeniz gerekiyor",Snackbar.LENGTH_INDEFINITE)
-                        .setAction("İzin Ver" , View.OnClickListener {
+                    Snackbar.make(
+                        view,
+                        "Galeriden Fotoraf seçmek için vermeniz gerekiyor",
+                        Snackbar.LENGTH_INDEFINITE
+                    ).setAction("İzin Ver", View.OnClickListener {
                             // İzin İsteyeceğimiz alan :
                             permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
                         }).show()
@@ -117,7 +141,8 @@ class UploadFragment : Fragment() {
             } else {
                 // izin var , telefonun galerisini aç :
                 // Galeriyi aç :
-                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                val intentToGallery =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                 activityResultLauncher.launch(intentToGallery)
             }
         }
@@ -126,14 +151,13 @@ class UploadFragment : Fragment() {
         binding.uploadBtn.setOnClickListener {
             val reference = storage.reference
             val gorselReferansi = reference.child("images").child("image.jpg")
-            if (selectedPicture != null){
+            if (selectedPicture != null) {
                 gorselReferansi.putFile(selectedPicture!!).addOnSuccessListener { uploadTask ->
                     // Url alma işlemi yapacağız
-                }
-                    .addOnFailureListener { exception ->
-                        // Yükleme esnasında hata fırlatıyorsa :
+                }.addOnFailureListener { exception ->
+                    // Yükleme esnasında hata fırlatıyorsa :
 
-                    }
+                }
             }
 
 
@@ -151,46 +175,51 @@ class UploadFragment : Fragment() {
     // onCreate içersinden çağırmak gerekiyor :
     private fun reginsterLauncher() {
         // initialize
-        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-            if (result.resultCode == RESULT_OK) {
-                val intentFromResult = result.data
-                if (intentFromResult != null) {
-                    selectedPicture = intentFromResult.data
-                    try {
-                        if (Build.VERSION.SDK_INT >= 28) {
-                            // SDK 28 ve üstü
-                            val source = ImageDecoder.createSource(
-                                requireActivity().contentResolver,
-                                selectedPicture!!
-                            )
-                            selectedBitmap = ImageDecoder.decodeBitmap(source)
-                            binding.imageView.setImageBitmap(selectedBitmap)
+        activityResultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val intentFromResult = result.data
+                    if (intentFromResult != null) {
+                        selectedPicture = intentFromResult.data
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28) {
+                                // SDK 28 ve üstü
+                                val source = ImageDecoder.createSource(
+                                    requireActivity().contentResolver, selectedPicture!!
+                                )
+                                selectedBitmap = ImageDecoder.decodeBitmap(source)
+                                binding.imageView.setImageBitmap(selectedBitmap)
 
-                        } else {
-                            // 27 ve altı için :
-                            selectedBitmap = MediaStore.Images.Media.getBitmap(
-                                requireActivity().contentResolver,
-                                selectedPicture
-                            )
-                            binding.imageView.setImageBitmap(selectedBitmap)
+                            } else {
+                                // 27 ve altı için :
+                                selectedBitmap = MediaStore.Images.Media.getBitmap(
+                                    requireActivity().contentResolver, selectedPicture
+                                )
+                                binding.imageView.setImageBitmap(selectedBitmap)
+                            }
+                        } catch (e: Exception) {
+                            e.printStackTrace()
                         }
-                    } catch (e: Exception) {
-                        e.printStackTrace()
                     }
                 }
             }
-        }
         // izin isteme :
-        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) {result ->
-            if (result) {
-                // izin verildi
-                // Galeriyi aç :
-                val intentToGallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-                activityResultLauncher.launch(intentToGallery)
-            } else {
-                // kullanici izini reddetti
-                Toast.makeText(requireContext(),"Fotoraf Seçimi İçin İzin Verilmesi Gerekiyor",Toast.LENGTH_LONG).show()
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+                if (result) {
+                    // izin verildi
+                    // Galeriyi aç :
+                    val intentToGallery =
+                        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    activityResultLauncher.launch(intentToGallery)
+                } else {
+                    // kullanici izini reddetti
+                    Toast.makeText(
+                        requireContext(),
+                        "Fotoraf Seçimi İçin İzin Verilmesi Gerekiyor",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
             }
-        }
     }
 }
